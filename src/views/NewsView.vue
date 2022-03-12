58710f5f8d1a8676f3ae3b529c72dc1cecaf14ae
Devbeombeom <krief19@gmail.com>
2022-03-13 00:00:28 +0900

    API 구현까지의 커밋 2022.03.13

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
-      news
+      <div v-for="user in users" v-bind:key ="user.id"> {{ user.title }}</div>
   </div>
 </template>
 
 <script>
+// import axios from 'axios';
+import { fetchNewsList } from '../api/index.js'
+
+
 export default {
+  data(){
+    return {
+      users: []
+    }
+  },
+  created() {
+    console.log('호출 전 :', this);
+    // var vm = this;
+    // axios.get('https://api.hnpwa.com/v0/news/1.json')
+    fetchNewsList()
+      .then(response => {
+        console.log('호출 후:', this);
+        this.users = response.data;
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+  }
 
 }
 </script>
